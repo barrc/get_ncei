a730e9da93864ec2a7daea2a84ff54819b6c7950
barrc <barr.colleen@epa.gov>
2020-09-14 17:11:50 -0700

    More flake8

--- src/get_coop_stations.py
+++ src/get_coop_stations.py
@@ -146,7 +146,7 @@ def assign_in_basins_attribute(basins, coops):
                             counter += 1
 
     message = (f"There are {counter} stations with a gap between the end of "
-                "BASINS and the start of the COOP record")
+               "BASINS and the start of the COOP record")
     print(message)
 
     return coops
@@ -158,9 +158,9 @@ def check_years(coops):
 
     Rules:
     1. If a station is in BASINS and is current, use the station
-    2. If a station is in BASINS and is not current, but there is no gap between
-       the BASINS end date and the C-HPD v2 start date,  use the station
-       as long as there is new data
+    2. If a station is in BASINS and is not current, but there is no gap
+       between the BASINS end date and the C-HPD v2 start date,  use the
+       station as long as there is new data
     3. If a stations is in BASINS and there is a gap between the BASINS
        end date and the C-HPD v2 start date, only use the station if there
        are at least 10 years of data from C-HPD v2
@@ -190,7 +190,7 @@ def check_years(coops):
             # else:
             #     coops_to_use.append(item)
         else:
-            # if not in BASINS, use if at least ten years of data from some sort of recent past
+            # if not in BASINS, use if >= ten years of data from recent past
             if item.station_id == '214546':
                 print('debug')
             if relativedelta(item.end_date, item.start_date).years >= 10:
@@ -203,7 +203,7 @@ def check_years(coops):
 
 def check_conditions_handled(id, coop_ids, expected_result):
     """
-    Checks if a station has been included or not included in coops_to_use properly
+    Checks if station is included in/excluded from coops_to_use as expected
     Returns True if matches expected_result
     """
     if expected_result:
@@ -212,10 +212,10 @@ def check_conditions_handled(id, coop_ids, expected_result):
         return id not in coop_ids
 
 
-
 def write_coop_stations_to_use(dummy_station):
 
-    with open(os.path.join('src', 'coop_stations_to_use.csv'), 'w', newline='') as file:
+    filename = os.path.join('src', 'coop_stations_to_use.csv')
+    with open(filename, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(asdict(dummy_station[0]).keys())
         for item in dummy_station:
@@ -229,7 +229,7 @@ def get_earliest_end_date(coops):
     """
 
     end_dates = [item.end_date for item in coops]
-    print(f'The earliest end date for a station in C-HPD v2 is {min(end_dates)}')
+    print(f'The earliest end date for a station is {min(end_dates)}')
 
 
 def get_latest_start_date(coops):
@@ -238,25 +238,27 @@ def get_latest_start_date(coops):
     Currently informational only
     """
     start_dates = [item.start_date for item in coops]
-    print(f'The latest start date for a station in C-HPD v2 is {max(start_dates)}')
+    print(f'The latest start date for a station is {max(start_dates)}')
 
 
 if __name__ == '__main__':
-    # if you have the most recent file, you can prevent re-downloading that file
-    # by commenting out the download_station_inventory_file() line and
-    # specifying the filepath directly below
+    # If you have the most recent station inventory file, you can prevent
+    # re-downloading that file by commenting out the
+    # download_station_inventory_file() line and specifying the filepath
+    # directly below
 
-    station_inventory_file = os.path.join('src', 'HPD_v02r02_stationinv_c20200909.csv')
-    # station_inventory_file = download_station_inventory_file()
+    station_inv = os.path.join('src', 'HPD_v02r02_stationinv_c20200909.csv')
+    # station_inv = download_station_inventory_file()
 
     basins_stations = common.read_basins_file()
-    coop_stations = read_coop_file(station_inventory_file)
+    coop_stations = read_coop_file(station_inv)
 
     coop_stations = assign_in_basins_attribute(basins_stations, coop_stations)
     coops_to_use = check_years(coop_stations)
 
     coops_to_use_ids = [x.station_id for x in coops_to_use]
 
+    # TODO come up with a better way of formatting this
     # 332974 -- in_basins, current, no break_with_basins --> use
     assert check_conditions_handled('332974', coops_to_use_ids, True)
     # 106174 -- in_basins, not current, no break_with_basins --> use
@@ -278,7 +280,7 @@ if __name__ == '__main__':
     get_earliest_end_date(coop_stations)
     get_latest_start_date(coop_stations)
 
-    # below function used for exploration; station/code matches deemed acceptable
+    # TODO fix this function
     # check_codes(basins_stations, coop_stations)
 
     # TODO write check_lat_lon function
