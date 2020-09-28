4353641a6d667d25d31f606f2fb77a7d4e4e8de1
Colleen Barr <barr.colleen@epa.gov>
2020-09-28 15:54:04 -0700

    Create get_isd.py

---
+++ src/get_isd.py
@@ -0,0 +1,21 @@
+
+
+# https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-hourly&dataTypes="TMP,AA1"&stations=72219013874&startDate=2019-01-01&endDate=2019-12-31
+
+# https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-hourly&dataTypes=AA1&stations=72219013874&startDate=2019-01-01&endDate=2019-12-31&format=json&options=includeAttributes:false"
+
+
+#FLD LEN: 3
+# LIQUID-PRECIPITATION occurrence identifier
+# The identifier that represents an episode of LIQUID-PRECIPITATION.
+# DOM: A specific domain comprised of the characters in the ASCII character set.
+# AA1 - AA4 An indicator of up to 4 repeating fields of the following items:
+# LIQUID-PRECIPITATION period quantity
+# LIQUID-PRECIPITATION depth dimension
+# LIQUID-PRECIPITATION condition code
+# LIQUID-PRECIPITATION quality code
+
+# "01,0000,9,5"
+
+if __name__ == '__main__':
+    pass
