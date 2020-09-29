132d59e03f8f08ed2ce8f22f339b848dba220d44
barrc <barr.colleen@epa.gov>
2020-09-29 16:32:10 -0700

    WIP

--- src/get_isd.py
+++ src/get_isd.py
@@ -1,4 +1,6 @@
-
+import json
+import os
+import requests
 
 # https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-hourly&dataTypes="TMP,AA1"&stations=72219013874&startDate=2019-01-01&endDate=2019-12-31
 
@@ -17,5 +19,35 @@
 
 # "01,0000,9,5"
 
+def quick_check(other_s_id):
+    url = 'https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-hourly&' + \
+        'dataTypes=AA1&stations=' + other_s_id + '&startDate=2019-01-01&endDate=2019-12-31' + \
+        '&format=json&options=includeAttributes:false'
+
+    r = requests.get(url)
+    assert r.status_code == 200
+
+    stuff = json.loads(r.content.decode())
+    # print(json.loads(r.content.decode()))
+
+    raw_filename = os.path.join('src', 'raw_isd_data', other_s_id + '.json')
+    with open(raw_filename, 'w') as file:
+        json.dump(stuff, file)
+
+def read_raw(station_id):
+    with open(os.path.join('src', 'raw_isd_data', station_id + '.json'), 'r') as file:
+        data = json.load(file)
+
+    report_types = [x['REPORT_TYPE'] for x in data]
+    print(set(report_types))
+    # FM-12 | SYNOP report of surface observation from a fixed land station
+    # FM-15 | METAR aviation routine weather report
+    # FM-16 | SPECI aviation selected special weather report
+    # SY-MT | Synoptic and METAR merged report
+    # SOM   | Summary of month report from U.S. ASOS or AWOS station
+    # SOD   | Summary of day report from U.S. ASOS or AWOS station
+
+
 if __name__ == '__main__':
-    pass
+    # quick_check('72219013874')
+    read_raw('72219013874')
