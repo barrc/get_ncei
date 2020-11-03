21266460a2049e2ac70689a368b8ae51ab45e630
barrc <barr.colleen@epa.gov>
2020-11-02 16:02:58 -0800

    WIP

--- src/homr.py
+++ src/homr.py
@@ -1,23 +1,60 @@
+import csv
 import json
+import os
 import requests
 
+import common
+from get_one_coop import str_date_to_datetime
 
-def get_codes(station_id):
-    base_url = 'https://www.ncdc.noaa.gov/homr/services/station/search?qid=COOP:'
+def get_specific_code(identifiers, id_type):
+    id_types = [item['idType'] for item in identifiers]
+
+    smo_id = None
+    wban_id = None
+    if id_type == 'COOP':
+        for x in identifiers:
+            if x['idType'] == 'WBAN':
+                wban_id = x['id']
+            elif x['idType'] == 'WMO':
+                smo_id = x['id']
+
+        return (smo_id, wban_id)
+
+    elif id_type == 'WMO' or id_type == 'WBAN':
+        for x in identifiers:
+            if x['idType'] == 'COOP':
+                return(x['id'])
+
+
+def get_codes(station_id, id_type):
+    base_url = 'https://www.ncdc.noaa.gov/homr/services/station/search?qid=' + id_type + ':'
     r = requests.get(base_url + station_id)
-    assert r.status_code == 200
+    try:
+        assert r.status_code == 200
+    except:
+        return None
 
     data = json.loads(r.content)
-    assert len(data['stationCollection']['stations']) == 1
+    try:
+        assert len(data['stationCollection']['stations']) == 1
+    except:
+        # pass
+        # print('except!')
+        # print(station_id)
+        print(data['stationCollection']['stations'])
 
-    identifiers = data['stationCollection']['stations'][0]['identifiers']
-    for x in identifiers:
-        if x['idType'] == 'WBAN':
-            wban_id = x['id']
-        elif x['idType'] == 'WMO':
-            smo_id = x['id']
+    out_codes = []
+    for x in data['stationCollection']['stations']:
+        identifiers = x['identifiers']
+        out_codes.append(get_specific_code(identifiers, id_type))
 
-    return (smo_id + '0' + wban_id)
+    return out_codes
+
+    # identifiers = data['stationCollection']['stations'][0]['identifiers']
+
+    # return(get_specific_code(identifiers, id_type))
+
+
 
 def quick_check(other_s_id):
     url = 'https://www.ncei.noaa.gov/access/services/data/v1?dataset=global-hourly&stations=' + other_s_id + '&startDate=1970-01-01&endDate=2019-12-31'
@@ -29,9 +66,59 @@ def quick_check(other_s_id):
 
 # stationCollection -> stations -> 0 -> identifiers -> idType -> WBAN + WMO
 
+def read_basins_not_in_chpd():
+    stations = []
+    with open(os.path.join('src', 'basins_not_in_chpd.csv'), 'r') as file:
+        coop_reader = csv.reader(file)
+        header = next(coop_reader)
+        for row in coop_reader:
+            if row[6] == 'True':
+                in_basins = True
+            else:
+                in_basins = False
+            if row[7] == 'True':
+                break_with_basins = True
+            else:
+                break_with_basins = False
+            stations.append(common.Station(row[0], row[1], row[2],
+                            str_date_to_datetime(row[3]),
+                            str_date_to_datetime(row[4]), row[5], row[6],
+                            in_basins, break_with_basins))
+
+    return stations
+
 if __name__ == '__main__':
+    pass
+    # with open('homr_codes.csv', 'w') as file:
+    #     coop_not_in_chpd_stations = read_basins_not_in_chpd()
+    #     for station in coop_not_in_chpd_stations:
+    #         s_id = station.station_id
+    #         print(s_id)
+    #         out_code = get_codes(s_id, 'COOP')
+    #         file.write(s_id)
+    #         file.write(',')
+    #         for x in out_code:
+    #             file.write(str(x[0]))
+    #             file.write(',')
+    #             file.write(str(x[1]))
+    #             file.write(',')
+    #         file.write('\n')
+
+
+
+
     # s_id = '090451'
-    # get_codes(s_id)
-    s_id = '457938'
-    wban_smo = get_codes(s_id)
-    quick_check(wban_smo)
+    # # s_id = '010272'
+    # print(get_codes(s_id, 'COOP'))
+    # s_id = '457938'
+    # # s_id =
+    # wban_smo = get_codes(s_id, 'COOP')
+    # print(wban_smo)
+    # quick_check(wban_smo)
+
+    # does it work the other way?
+    # print(get_codes('72785', 'WMO'))
+
+    # s_id = '049099' # COOP TWENTYNINE PALMS
+    # # get_codes(s_id, 'COOP')
+    # get_codes('690170', 'WMO')
