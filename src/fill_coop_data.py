006fcf4f60f9dc54973c703c0df348394e3100ae
barrc <barr.colleen@epa.gov>
2021-04-23 10:27:38 -0700

    Add script to fill C-HPD stations with NLDAS/GLDAS

---
+++ src/fill_coop_data.py
@@ -0,0 +1,409 @@
+import csv
+import datetime
+import os
+import random
+
+import matplotlib.pyplot as plt
+import requests
+
+import common
+import get_coop_precip
+
+
+class NLDAS:
+    DEGREES_PER_GRID_CELL = 1.0 / 8.0
+    WESTMOST_GRID_EDGE = -125.0
+    SOUTHMOST_GRID_EDGE = 25.0
+    WESTMOST_GRID_CENTER = WESTMOST_GRID_EDGE + DEGREES_PER_GRID_CELL / 2.0
+    SOUTHMOST_GRID_CENTER = SOUTHMOST_GRID_EDGE + DEGREES_PER_GRID_CELL / 2.0
+
+
+def grid_cell_from_lat_lon(lat, lon):
+    x = int(round((lon - NLDAS.WESTMOST_GRID_CENTER)
+                  / NLDAS.DEGREES_PER_GRID_CELL))
+    y = int(round((lat - NLDAS.SOUTHMOST_GRID_CENTER)
+                  / NLDAS.DEGREES_PER_GRID_CELL))
+    return str(x), str(y)
+
+
+def get_nldas_data(data_type, start_date_str, end_date_str, x_str, y_str):
+    precip_url = "https://hydro1.sci.gsfc.nasa.gov/daac-bin/access/" \
+                  + "timeseries.cgi?variable=NLDAS:NLDAS_FORA0125_H.002:" \
+                  + data_type + "&startDate=" + start_date_str \
+                  + "&endDate=" + end_date_str \
+                  + "&location=NLDAS:X" + x_str + "-Y" + y_str + "&type=asc2"
+
+    r = requests.get(precip_url)
+
+    return r.content
+
+
+def get_datetime_date(str_date):
+    split_date = str_date.split('-')
+    datetime_date = datetime.datetime(
+        int(split_date[0]), int(split_date[1]), int(split_date[2][0:2]),
+        int(split_date[2][-2:]))
+
+    return datetime_date
+
+def get_gldas_data(data_type, start_date, end_date, lat, lon):
+    # start_date = get_datetime_date(start_date_str)
+    # end_date = get_datetime_date(end_date_str)
+
+    gldas_21_start_date = datetime.datetime(2000, 1, 1)
+
+    gldas_20 = 'GLDAS2:GLDAS_NOAH025_3H_v2.0:'
+    gldas_21 = 'GLDAS2:GLDAS_NOAH025_3H_v2.1:'
+
+    if start_date > gldas_21_start_date:
+        conditions = [None, gldas_21]
+    elif end_date < gldas_21_start_date:
+        conditions = [gldas_20, None]
+    else:
+        conditions = [gldas_20, gldas_21]
+
+    data = [None, None]
+    for (index, condition) in enumerate(conditions):
+        if condition:
+            if condition[-4:-1] == '2.0':
+                start_date_str = f'{start_date:%Y-%m-%dT00}'
+                if end_date > gldas_21_start_date:
+                    end_date_str = '2000-01-01T00'
+                else:
+                    end_date_str = f'{end_date + datetime.timedelta(days=2):%Y-%m-%dT00}'
+            elif condition[-4:-1] == '2.1':
+                if start_date > gldas_21_start_date:
+                    start_date_str = f'{start_date:%Y-%m-%dT00}'
+                else:
+                    start_date_str = '2000-01-01T00'
+                end_date_str = f'{end_date + datetime.timedelta(days=2):%Y-%m-%dT00}'
+
+                # start_date_str = '2000-01-01T00'
+                # end_date_str = '2007-01-01T00'
+            precip_url = "https://hydro1.gesdisc.eosdis.nasa.gov/daac-bin/access/" \
+                + "timeseries.cgi?variable=" + condition \
+                + data_type + "&startDate=" + start_date_str \
+                + "&endDate=" + end_date_str \
+                + "&location=GEOM:POINT(" + lon + ",%20" + lat + ")" \
+                + "&type=asc2"
+
+            r = requests.get(precip_url)
+            data[index] = r.content
+
+    return data
+
+
+
+def process_nldas_data(data):
+    metadata = {}
+    nldas_dict = {}
+
+    split_data = data.decode().split('\n')
+    for line in split_data:
+        ls = line.strip()
+        if "error" in ls.lower():
+            raise ValueError(ls.lower())
+        date_time_data = ls.split()
+        if (len(date_time_data) == 3 and len(date_time_data[0]) == 10
+                and len(date_time_data[1]) == 3):
+            ymd = date_time_data[0].split('-')
+            local_date = datetime.datetime(
+                             int(ymd[0]), int(ymd[1]), int(ymd[2]),
+                             int(date_time_data[1][:-1]))
+            nldas_dict[local_date] = float(date_time_data[2])/25.4
+        else:
+            meta_item = ls.split('=')
+            if len(meta_item) == 2:
+                metadata[meta_item[0]] = meta_item[1]
+
+    return nldas_dict
+
+
+def process_gldas_data(data):
+    metadata = {}
+    nldas_dict = {}
+    for x in data:
+        if x:
+
+            split_data = x.decode().split('\n')
+            for line in split_data:
+                ls = line.strip()
+                if "error" in ls.lower():
+                    raise ValueError("GLDAS: Error getting precipitation data")
+                date_time_data = ls.split()
+                if (len(date_time_data) == 2 and len(date_time_data[0]) == 19):
+                    ymd = date_time_data[0].split('-')
+                    local_date = datetime.datetime(
+                                    int(ymd[0]), int(ymd[1]), int(ymd[2][0:2]),
+                                    int(ymd[2][3:5]))
+                    nldas_dict[local_date] = float(date_time_data[1])/25.4*3600
+                else:
+                    meta_item = ls.split('=')
+                    if len(meta_item) == 2:
+                        metadata[meta_item[0]] = meta_item[1]
+
+    return nldas_dict
+
+def read_data(filename):
+
+    with open(filename, 'r') as file:
+        precip_data = file.readlines()
+
+    return [item.split() for item in precip_data]
+
+
+def get_missing_dates(data_1):
+    missing_dates = []
+    for x in data_1:
+        if x[-1] == '-9999':
+            try:
+                missing_dates.append(
+                    datetime.datetime(int(x[1]), int(x[2]), int(x[3]),
+                                      int(x[4])))
+            except ValueError:
+                missing_dates.append(
+                    datetime.datetime(int(x[1]), int(x[2]), int(x[3])) +
+                    datetime.timedelta(days=1))
+
+    return missing_dates
+
+
+def adjust_dates(nldas, utc):
+    nldas_dict = {}
+    for x in nldas:
+        adjusted_date = x + datetime.timedelta(hours=utc)
+        nldas_dict[adjusted_date] = nldas[x]
+
+    return nldas_dict
+
+
+def get_corresponding_nldas(missing_dates, nldas_data):
+
+    first_nldas_date = list(nldas_data.items())[0][0]
+    # this will be approximately 1/1/1979, with a different hour depending
+    # on the UTC offset
+    # for EST, the first available date should be 1979/1/1 19:00
+
+    missing = {}
+
+    for missing_date in missing_dates:
+        if missing_date >= first_nldas_date:
+            missing[missing_date] = nldas_data[missing_date]
+
+    return first_nldas_date, missing
+
+def get_corresponding_gldas(missing_dates, gldas_data):
+
+    first_gldas_date = list(gldas_data.items())[0][0]
+    # this will be approximately 1/1/1979, with a different hour depending
+    # on the UTC offset
+    # for EST, the first available date should be 1979/1/1 19:00
+
+    missing = {}
+
+    gldas_keys = list(gldas_data.keys())
+
+    for missing_date in missing_dates:
+        if missing_date >= first_gldas_date:
+            if missing_date in gldas_keys:
+                missing[missing_date] = gldas_data[missing_date]
+            else:
+                subtracted_date = missing_date - datetime.timedelta(hours = 1)
+                if subtracted_date in gldas_keys:
+                    missing[missing_date] = gldas_data[subtracted_date]
+                else:
+                    subtracted_date = missing_date - datetime.timedelta(hours = 2)
+                    if subtracted_date in gldas_keys:
+                        missing[missing_date] = gldas_data[subtracted_date]
+                    else:
+                        raise ValueError
+
+
+    return first_gldas_date, missing
+
+
+def fill_data(missing, coop_data, first_nldas_date):
+
+    filled_data = []
+    for x in coop_data:
+        try:
+            local_date = datetime.datetime(
+                int(x[1]), int(x[2]), int(x[3]), int(x[4]))
+        except ValueError:
+            local_date = datetime.datetime(
+                int(x[1]), int(x[2]), int(x[3])) + datetime.timedelta(days=1)
+
+        if local_date >= first_nldas_date:
+            if x[-1] == '-9999':
+
+                nldas_precip = missing[local_date]
+                local_thing = x[0:-1]
+                local_thing.append(nldas_precip)
+                filled_data.append(local_thing)
+            else:
+                filled_data.append(x)
+
+    return filled_data
+
+
+def get_dict(input_dict):
+    output_dict = {}
+    for x in input_dict:
+        try:
+            local_date = datetime.datetime(
+                int(x[1]), int(x[2]), int(x[3]), int(x[4]))
+        except ValueError:
+            local_date = datetime.datetime(
+                int(x[1]), int(x[2]), int(x[3])) + datetime.timedelta(days=1)
+        output_dict[local_date] = float(x[-1])
+
+    return output_dict
+
+
+
+def get_offset(item):
+    station_inv_file = os.path.join(
+        'src', 'HPD_v02r02_stationinv_c20200909.csv')
+
+    with open(station_inv_file, 'r') as csv_file:
+        station_inv_reader = csv.reader(csv_file)
+        header = next(station_inv_reader)
+        for row in station_inv_reader:
+            if row[0] == item.station_id:
+                utc_offset = int(row[8])
+
+    return utc_offset
+
+def nldas_routine(coop_filename, station):
+    coop_precip_data = read_data(coop_filename)
+
+    x_grid, y_grid = grid_cell_from_lat_lon(
+        float(station.latitude), float(station.longitude))
+
+    start_date_str = f'{station.start_date_to_use:%Y-%m-%dT24}'
+    end_date_str = f'{station.end_date_to_use + datetime.timedelta(days=1):%Y-%m-%dT24}'
+
+    raw_nldas_data = get_nldas_data(
+        'APCPsfc', start_date_str, end_date_str, x_grid, y_grid)
+    # try:
+    nldas_precip_data = process_nldas_data(raw_nldas_data)
+    # except ValueError:
+        # print(station.station_id)
+
+    # data returned is in UTC
+    # for COOP, adjust this by utc_offset
+    adjusted_nldas_data = adjust_dates(nldas_precip_data, offset)
+
+    coop_missing_dates = get_missing_dates(coop_precip_data)
+
+    first_missing_date, missing_dict = get_corresponding_nldas(
+        coop_missing_dates, adjusted_nldas_data)
+
+    filled_coop_data = fill_data(
+        missing_dict, coop_precip_data, first_missing_date)
+
+    out_file = os.path.join(common.DATA_BASE_DIR, 'filled_coop_data', station.station_id + '.dat')
+
+    with open(out_file, 'w') as file:
+        for item in filled_coop_data:
+            if type(item[6]) == str:
+                str_precip = item[6]
+            elif item[6] == 0:
+                str_precip = '0.0'
+            else:
+                str_precip = str(round(item[6], 3))
+
+            if str_precip == '0.0':
+                pass
+            else:
+                to_file = f'{item[0]}\t{item[1]}\t{item[2]}\t{item[3]}\t{item[4]}\t{item[5]}\t{str_precip}\n'
+                file.write(to_file)
+
+def gldas_routine(coop_filename, station):
+    coop_precip_data = read_data(coop_filename)
+
+    raw_gldas_data = get_gldas_data(
+        'Rainf_tavg', station.start_date_to_use, station.end_date_to_use,
+        str(station.latitude), str(station.longitude))
+
+    try:
+        gldas_precip_data = process_gldas_data(raw_gldas_data)
+    except ValueError:
+        print(station.station_id)
+
+    no_gldas_date = datetime.datetime(2020, 1, 1, 0, 0)
+    if no_gldas_date not in list(gldas_precip_data.keys()):
+        gldas_precip_data[no_gldas_date] = 0.0
+    else:
+        print(f'{station.station_id} has data there')
+
+    adjusted_gldas_data = adjust_dates(gldas_precip_data, offset)
+    coop_missing_dates = get_missing_dates(coop_precip_data)
+
+    first_missing_date, missing_dict = get_corresponding_gldas(
+        coop_missing_dates, adjusted_gldas_data)
+
+    filled_coop_data = fill_data(
+        missing_dict, coop_precip_data, first_missing_date)
+
+    out_file = os.path.join(common.DATA_BASE_DIR, 'filled_coop_data', station.station_id + '.dat')
+
+    with open(out_file, 'w') as file:
+        for item in filled_coop_data:
+            if type(item[6]) == str:
+                str_precip = item[6]
+            elif item[6] == 0:
+                str_precip = '0.0'
+            else:
+                str_precip = str(round(item[6], 3))
+
+            if str_precip == '0.0':
+                pass
+            else:
+                to_file = f'{item[0]}\t{item[1]}\t{item[2]}\t{item[3]}\t{item[4]}\t{item[5]}\t{str_precip}\n'
+                file.write(to_file)
+
+
+
+
+if __name__ == '__main__':
+    coop_stations_to_use = common.get_stations('coop_stations_to_use.csv')
+
+    with open(os.path.join('src', 'coop_stations_to_use.csv'), 'r') as file:
+        data = file.readlines()
+
+    split_data = [item.strip('\n').split(',') for item in data]
+
+    header = split_data.pop(0)
+
+    for x in split_data:
+        for coop in coop_stations_to_use:
+            if coop.station_id == x[0]:
+                station_ = coop
+                offset = get_offset(station_)
+
+        c_filename = os.path.join(
+            common.DATA_BASE_DIR, 'processed_coop_data',
+            station_.station_id + '.dat')
+
+        assert os.path.exists(c_filename)
+
+        if 25 < float(station_.latitude) < 53 and -125 < float(station_.longitude) < -63:
+            fill_type = 'nldas'
+        else:
+            fill_type = 'gldas'
+
+        if fill_type == 'nldas':
+            try:
+                nldas_routine(c_filename, station_)
+            except ValueError as e:
+                if 'water' in e.args[0]:
+                    gldas_routine(c_filename, station_)
+                else:
+                    pass
+                    # probably just an error on NLDAS side; try again later
+
+        elif fill_type == 'gldas':
+            gldas_routine(c_filename, station_)
+
+
